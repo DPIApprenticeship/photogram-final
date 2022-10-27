9c6403d6f62cd9b8c4096b0113547a82fd6cc882
mpuchyr <matt.puchyr@gmail.com>
2022-10-27 15:34:10 +0000

    Create users/index and related routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def index
+    render({:template => "users/index.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "users", :action => "index"})
+  get("/users", {:controller => "users", :action => "index"})
   # Routes for the Like resource:
 
   # CREATE
